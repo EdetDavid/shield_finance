8907db335568e8035ce19a35ccc6442e8335163f
dvooskid <dvooskid@localhost.localdomain>
2023-12-10 21:05:38 +0100

    first commit

--- banking_system/urls.py
+++ banking_system/urls.py
@@ -7,14 +7,11 @@ from core.views import HomeView
 
 
 urlpatterns = [
-    path('', HomeView.as_view(), name='home'),
-    path('accounts/', include('accounts.urls', namespace='accounts')),
-    path('admin/', admin.site.urls),
-    path(
-        'transactions/',
-        include('transactions.urls', namespace='transactions')
-    ),
-    path('payments/', include('payments.urls', namespace='payments'))
+    path("", HomeView.as_view(), name="home"),
+    path("accounts/", include("accounts.urls", namespace="accounts")),
+    path("admin/", admin.site.urls),
+    path("transactions/", include("transactions.urls", namespace="transactions")),
+    path("payments/", include("payments.urls", namespace="payments")),
 ]
 urlpatterns += staticfiles_urlpatterns()
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
